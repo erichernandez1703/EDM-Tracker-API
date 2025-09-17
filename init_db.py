3de89e38cec286551cb85e74cc9627e7a335dc9c
Eric Hernandez <141759184+erichernandez1703@users.noreply.github.com>
2025-09-17 17:43:19 -0400

    routes and schemas

--- init_db.py
+++
@@ -1,4 +0,0 @@
-from database import engine  # adjust if your path is different
-from models import Base
-
-Base.metadata.create_all(bind=engine)
--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from. import models, database
-
-app = FastAPI() #makes the app object - attach functions to it
-
-@app.get("/") 
-def root():
-    return {"message": "EDM Artist Tracker API"}
